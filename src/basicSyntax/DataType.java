a44c6195c002efc72ce94b4164d60610b0d9660e
KAZUKI-TANAKA-0116 <t.tanaka.cingroup2012gmail.com>
2024-09-22 22:35:59 +0900

    データ型の実装

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	    System.out.println("こんにちは" + "私は" + "Geek君です。");
+	    
+	    System.out.println(10);
+	    System.out.println(2014);
+	    System.out.println(3.12);
+	    
+	    System.out.println(true);
+	}
+
+}
