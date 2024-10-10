c3bcdb2effb2e4c57fa5c6a4b440514fd5a49bec
KAZUKI-TANAKA-0116 <t.tanaka.cingroup2012gmail.com>
2024-10-10 22:48:04 +0900

    再度、インデント修正しました！

--- JavaBasic/src/basicSyntax/ComparisonLogicalOperators.java
+++ JavaBasic/src/basicSyntax/ComparisonLogicalOperators.java
@@ -3,32 +3,32 @@ package basicSyntax;
 public class ComparisonLogicalOperators {
 
 	public static void main(String[] args) {
+		
 		int a = 15;
 		int b = 10;
 		boolean isGreaterA = (a > b);
 
-		System.out.println(isGreaterA);
-		
+		System.out.println(isGreaterA);//true
+	
 		boolean isSunny = true;
 		boolean isWarm = true;
 		
 		boolean andResultA = (isSunny && isWarm) ;
-		System.out.println(andResultA);
+		System.out.println(andResultA);//true
 		
 		int x = 5;
 		int y = 10;
-		
+
 		boolean andResultB = (x > 0 && y % 2 == 0);
-		
-		System.out.println(andResultB);
+
+		System.out.println(andResultB); // true
 		
 		boolean haspermission;
 		haspermission = false;
-		
+	    
 		boolean notResult = !(haspermission);
-		
+	    
 		System.out.println(notResult);
-		
 	}
-	
+
 }
