39b593722503ecaf1068fc928a3c4257aa8f79a9
KAZUKI-TANAKA-0116 <t.tanaka.cingroup2012gmail.com>
2024-10-16 12:09:31 +0900

    メソッド課題

---
+++ src/basicSyntax/MethodPractice.java
@@ -0,0 +1,43 @@
+package basicSyntax;
+
+public class MethodPractice {
+	
+	public static void main(String[] args) {
+		
+		int a = 50;
+		int b = 10;
+        
+        
+		int result1 = add(a, b);
+		allResult(result1);
+		
+		int result2 = subtract(a, b);
+		allResult(result2);
+		
+		int result3 = multiply(a, b);
+		allResult(result3);
+		
+		int result4 = divide(a, b);
+		allResult(result4);
+		}
+	
+	public static int add(int a, int b) {
+		return a + b;
+		}
+	
+	public static int subtract(int a, int b) {
+		return a - b;
+		}
+	
+	public static int multiply(int a, int b) {
+		return a * b;
+		}
+	
+	public static int divide(int a, int b) {
+		return a % b;
+		}
+	
+	public static void allResult(int result) {
+		System.out.println("計算結果は" + result + "です。");
+		}
+}
