cb1c84f99ec4023b8528e2dd964bfbb3509d9c0d
KAZUKI-TANAKA-0116 <t.tanaka.cingroup2012gmail.com>
2024-10-17 16:10:11 +0900

    クラス

---
+++ JavaBasic/GeekIntroduction.java
@@ -0,0 +1,11 @@
+package basicSyntax;
+
+public class GeekIntroduction {
+	public static void greeting(int age, double height) {
+		return;
+	}
+
+	public static void specialSkill(int num1, int num2) {
+		return;
+	}
+}
