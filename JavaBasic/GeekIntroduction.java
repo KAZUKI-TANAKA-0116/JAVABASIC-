660ff080ebe6b1a00d50875fb3b4d0d1c91cf57c
KAZUKI-TANAKA-0116 <t.tanaka.cingroup2012gmail.com>
2024-10-17 21:32:33 +0900

    インスタンス

--- JavaBasic/GeekIntroduction.java
+++ JavaBasic/GeekIntroduction.java
@@ -1,11 +1,39 @@
 package basicSyntax;
 
 public class GeekIntroduction {
-	public static void greeting(int age, double height) {
-		return;
+
+	public void greeting(int age, double height) {
+		System.out.println("こんにちは! 私はGeek君でごわす。");
+		System.out.println("年は" + age + "でごわす。");
+		System.out.println("身長は" + height + "でごわす。");
 	}
 
-	public static void specialSkill(int num1, int num2) {
-		return;
+	public void specialSkill(int num1, int num2) {
+		for (num1 = 5; num1 <= num2; num1++) {
+
+			if (num1 < 1 || num2 < 0) {
+				System.out.println("num1とnum2は0より大きい値にしてください。");
+				break;
+			} else if (num2 <= num1) {
+				System.out.println("num2はnum1より大きい値にしてください。");
+				break;
+			} else if (300 < num1 || 300 < num2) {
+				System.out.println("num1とnum2は300以下にしてください。");
+				break;
+			} else if (num1 % 3 == 0 && num1 % 5 == 0) {
+				System.out.println(num1 + "は3倍数かつ5の倍数です。");
+				continue;
+			} else if (num1 % 3 == 0) {
+				System.out.println(num1 + "は3倍数です。");
+				continue;
+			} else if (num1 % 5 == 0) {
+				System.out.println(num1 + "は5の倍数です。");
+				continue;
+			} else {
+				System.out.println(num1);
+
+			}
+		}
+
 	}
 }
--- JavaBasic/Main.java
+++ JavaBasic/Main.java
@@ -1,48 +1,11 @@
 package basicSyntax;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ		
-		int age = 18;
-		double height = 169.5;
+		GeekIntroduction geek = new GeekIntroduction();
 
-		GeekIntroduction.greeting(age, height);
+		geek.greeting(18, 169.5);
 
-		System.out.println("こんにちは！私はGeek君でごわす。");
-		System.out.println("年は" + age + "でごわす。");
-		System.out.println("身長は" + height + "でごわす。");
-
-		int num1 = 5;
-		int num2 = 100;
-
-		GeekIntroduction.specialSkill(num1, num2);
-
-		for (num1 = 5; num1 <= num2; num1++) {
-
-			if (num1 < 1 || num2 < 0) {
-				System.out.println("num1とnum2は0より大きい値にしてください。");
-				break;
-			} else if (num2 <= num1) {
-				System.out.println("num2はnum1より大きい値にしてください。");
-				break;
-			} else if (300 < num1 || 300 < num2) {
-				System.out.println("num1とnum2は300以下にしてください。");
-				break;
-			} else if (num1 % 3 == 0 && num1 % 5 == 0) {
-				System.out.println(num1 + "は3倍数かつ5の倍数です。");
-				continue;
-			} else if (num1 % 3 == 0) {
-				System.out.println(num1 + "は3倍数です。");
-				continue;
-			} else if (num1 % 5 == 0) {
-				System.out.println(num1 + "は5の倍数です。");
-				continue;
-			} else {
-				System.out.println(num1);
-
-			}
-		}
+		geek.specialSkill(5, 100);
 	}
-
 }
